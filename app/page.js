7c6d53119a53fc51aeb7444287a00b4f55d570cd
mdmubin7547 <mdmubin7547@gmail.com>
2026-02-27 15:31:01 +0600

    Create Home component for main page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Diamond TopUp 🔥</h1>
+      <p>God level site is starting...</p>
+    </main>
+  );
+}
