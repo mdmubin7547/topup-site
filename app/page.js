67a5fde7b3a880c75ef124faab20e27c2e44c4b7
mdmubin7547 <mdmubin7547@gmail.com>
2026-02-28 12:45:58 +0600

    Fix import statement for PacksGrid component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,2 @@
 import Hero from "../components/sections/Hero";
 import PacksGrid from "../components/sections/PacksGrid";
-
-export default function Home() {
-  return (
-    <>
-      <Hero />
-      <PacksGrid />
-    </>
-  );
-}
